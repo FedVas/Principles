7529e240af5b69b86352eb508446ef176634134e
Fedor <atlz89@mail.ru>
2023-06-07 19:44:20 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+}
+//name": "Иван",
+//  "passport": "4444 № 44444444",
+//  "patronymic": "Иванович",
+//  "phone": "+7 (999)-999-99-99",
+//  "surname": "Иванов"
